24b6554901e4112de363cd155e8522b5af4ddf35
eliujohn <johnacevedo510@gmail.com>
2024-06-26 13:02:15 -0400

    Create 20240625_programming_questionnaire.py

---
+++ 20240625_programming_questionnaire.py
@@ -0,0 +1,11 @@
+def main():
+name = "professor nedd"
+print("we want to know if you like programming!")
+print()
+print("do you like programming" + name + "?")
+answer = input()
+print("great! you said "+ answer + "?")
+print("Lets start learning some python today")
+
+if __name__ == "__main__":
+   main()
