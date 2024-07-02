3774bed0ee223526d75421226ff428755182aafa
eliujohn <johnacevedo510@gmail.com>
2024-07-02 12:57:33 -0400

    Create my_name2.py

---
+++ my_name2.py
@@ -0,0 +1,8 @@
+import my_name1 as n1
+def main():
+    print("welcome to my name2 program")
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    n1.get_largest(num1, num2)
+if __name__ == "__main__":
+    main()
