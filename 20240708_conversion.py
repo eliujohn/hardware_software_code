83b75953adf3d6e6d3df88418f7ef5af6e1dc71b
eliujohn <johnacevedo510@gmail.com>
2024-07-08 12:58:57 -0400

    Create 20240708_conversion.py

---
+++ 20240708_conversion.py
@@ -0,0 +1,9 @@
+def conversation(name):
+    print ("Hi {} ".format(name.capitalize()))
+    print ("Welcome to my conversation program!!!")
+    print ("Glad to have you in Hardware and Software!!!")
+def main():
+    name = conversation(input("What is your name?"))
+    print ("Nice talking with you {}".format(name))
+if __name__ == "__main__":
+    main()
